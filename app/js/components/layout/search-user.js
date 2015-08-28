2b67fbf666425197e136c477ad7d2eb582a569b3
PaquitoSoft <paquitosoftware@gmail.com>
2015-08-28 18:58:47 +0200

    local dependency fix

--- app/js/components/layout/search-user.js
+++ app/js/components/layout/search-user.js
@@ -1,6 +1,7 @@
 import Ractive from 'ractive';
+import { RouterManager } from 'ps-ractive-router';
 import Template from '../../../views/layout/search-user.html';
-import * as router from '../../plugins/router';
+// import * as router from '../../plugins/router';
 
 var SearchGithub = Ractive.extend({
 	isolated: true,
@@ -9,8 +10,10 @@ var SearchGithub = Ractive.extend({
 	oninit() {
 		this.on('searchUser', (rEvent) => {
 			rEvent.original.preventDefault();
-			router.navTo(`/user/${rEvent.context.query}`);
-			this.set('query', '');
+			if (rEvent.context.query.trim().length) {
+				RouterManager.navTo(`/user/${rEvent.context.query}`);
+				this.set('query', '');
+			}
 		});
 	},
 
